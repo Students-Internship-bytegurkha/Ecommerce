8a2abc84b7766c9aa42242d1a7b379f45bd18962
florence0919 <laksamflorence19@gmail.com>
2026-03-30 08:26:32 +0545

    update homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from '../components/layout/Navbar'
 import Sidebar from '../components/layout/Sidebar'
+import RightDashboard from '../components/layout/RightDashboard'
+import Productlist from './Productlist'
 
 
 const Homepage = () => {
   return (
-    <div className='bg-gray-100 w-screen h-screen flex p-3'>
-        <Sidebar/>
-        <Navbar/>
+    <div className='bg-gray-100 w-screen h-screen  p-3'>
+       
+       <Navbar/>
+
+      <div className='flex'>
+        <Productlist/>
+
+    <RightDashboard/>
+
+      </div>
       
     </div>
   )
